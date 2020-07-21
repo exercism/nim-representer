76ec03f5734af4fa80277f7020deb327de1cb1d5
Yoni Fihrer <yoni@fihrer.com>
2020-07-21 13:07:40 -0400

    Feature(representer): conform to specifcation of the interface
    This include:
      - representer:
        - receive slug and directory as CLI compilte-time args
        - switch keys and values for mapping
        - write `representation.txt` & `mapping.json` to directory
      - mapping:
        - add support for Jsonifying the switched map

---
+++ bin/generate.sh
@@ -0,0 +1,7 @@
+set -e
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "slug and solution directory must be present"
+    exit 1
+fi
+nim c --outdir:bin/ -d:slug="$1" -d:dir="$2" src/representer
--- src/representer.nim
+++ src/representer.nim
@@ -1,6 +1,8 @@
 import macros, os, sequtils, strutils
 import representer/[mapping, normalizations]
 
+proc switchKeysValues*(map: IdentMap): OrderedTable[string, NormalizedIdent] =
+  toSeq(map.pairs).mapIt((it[1], it[0])).toOrderedTable
 
 proc createRepresentation*(fileName: string): tuple[tree: NimNode, map: IdentMap] =
   var map: IdentMap
@@ -8,11 +10,16 @@ proc createRepresentation*(fileName: string): tuple[tree: NimNode, map: IdentMap
   result = (tree: code.normalizeStmtList(map), map: map)
 
 
+const dir {.strdefine.} = ""
+const slug {.strdefine.} = ""
+const underSlug = slug.replace('-', '_')
+
 when isMainModule:
   import json
   static:
-    const path {.strdefine.} = "../../Exercism/nim/two-fer/two_fer.nim" ##\
-    ## The path to the file to create representation for
-    ## Can be invoked with -d:path=<PATH-TO-FILE>
-    let (tree, map) = createRepresentation path
-    echo (%*{"map": map, "tree": tree.repr}).pretty
+    let (tree, map) = createRepresentation(dir / underSlug & ".nim")
+    let finalMapping = map.switchKeysValues
+    echo (%*{"map": finalMapping, "tree": tree.repr}).pretty
+    when defined(dir):
+      writeFile(dir / "representation.txt", tree.repr)
+      writeFile(dir / "mapping.json", $(%finalMapping))
