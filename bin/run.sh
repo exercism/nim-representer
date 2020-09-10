eaa79a6d9aa96cc920c51765030ee006d0602ef3
Yoni Fihrer <yoni@fihrer.com>
2020-09-10 16:00:55 +0300

    Credit script arg checking to rust

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,8 @@
 set -e
 
+# Thanks rust
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-    echo "slug and solution directory must be present"
+    echo "slug, solution directory and output directory must be present"
     exit 1
 fi
-nim c --outdir:bin/ -d:slug="$1" -d:dir="$2" src/representer
+nim c --outdir:bin/ -d:slug="$1" -d:inDir="$2" -d:outDir="$3" src/representer
