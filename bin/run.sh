0e19c791ff2dd2241db34a6e124745f5e2b4c780
Yoni Fihrer <yoni@fihrer.com>
2020-11-01 01:16:57 +0200

    Fix(cli): force recompilation
    Being as the representer has to be recompiled everytime there is a new
    normalization, use the `-f` compilation flag to force recompilation
    every time.

--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,4 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     echo "slug, solution directory and output directory must be present"
     exit 1
 fi
-nim c --outdir:bin/ -d:slug="$1" -d:inDir="$2" -d:outDir="$3" src/representer
+nim c -f --outdir:bin/ -d:slug="$1" -d:inDir="$2" -d:outDir="$3" src/representer
