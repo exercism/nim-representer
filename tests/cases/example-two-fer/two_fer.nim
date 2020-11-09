bbd7c40c2e980b9efbae7b729f5a30f5ff331f0e
Yoni Fihrer <yoni@fihrer.com>
2020-11-09 23:19:14 +0200

    Test: Create basic two-fer example for testing

---
+++ tests/cases/example-two-fer/two_fer.nim
@@ -0,0 +1,3 @@
+## https://github.com/exercism/nim/blob/516068d7f2259ecd6cff46beb9ef7659c7bbd45a/exercises/two-fer/example.nim
+proc twoFer*(name = "you"): string =
+  "One for " & name & ", one for me."
